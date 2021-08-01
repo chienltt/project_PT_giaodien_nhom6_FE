3d49fd7dc2763d38dabb7453beb1f11b299261ab
baobaone2727 <baobaone2727@users.noreply.github.com>
2021-08-01 10:35:13 +0700

    Nhat-HomePage

--- src/app/homepage/Footer.js
+++ src/app/homepage/Footer.js
@@ -7,11 +7,17 @@ const Footer = (props)=>{
         <div className="container-fluid pb-0 mb-0 justify-content-center text-light footer ">
             <footer>
                 <div className="row my-5 justify-content-center py-5">
-                    <div className="col-11">
+                    <div className="col-10">
                         <div className="row ">
                             <div className="col-xl-8 col-md-4 col-sm-4 col-12 my-auto mx-auto a">
-                                <h3 className="text-muted mb-md-0 mb-5 bold-text">Pepper.</h3>
+                                <h3 className="text-muted mb-md-0 mb-5 bold-text">Old items exchange</h3>
+                                <p>This is a website designed to exchange your old stuff with others.
+                                    Our aim is to help people save money and minimize the amount of waste to the environment.  </p>
+                                <h4>What is old for others might be new for us!
+                                </h4>
+
                             </div>
+
                             <div className="col-xl-2 col-md-4 col-sm-4 col-12">
                                 <h6 className="mb-3 mb-lg-4 bold-text "><b>MENU</b></h6>
                                 <ul className="list-unstyled">
@@ -23,9 +29,11 @@ const Footer = (props)=>{
                             </div>
                             <div className="col-xl-2 col-md-4 col-sm-4 col-12">
                                 <h6 className="mb-3 mb-lg-4 text-muted bold-text mt-sm-0 mt-5"><b>ADDRESS</b></h6>
-                                <p className="mb-1">605, RATAN ICON BUILDING</p>
-                                <p>SEAWOODS SECTOR</p>
+                                <p className="mb-1">144 Xuân Thủy, Mai Dịch, Cầu Giấy, Hà Nội
+                                </p>
+
                             </div>
+
                         </div>
                         <div className="row ">
                             <div
@@ -35,16 +43,13 @@ const Footer = (props)=>{
                                     className="fa fa-linkedin-square" aria-hidden="true"></i></span> <span
                                     className="mx-2"><i className="fa fa-twitter" aria-hidden="true"></i></span> <span
                                     className="mx-2"><i className="fa fa-instagram" aria-hidden="true"></i></span></p>
-                                <small className="rights"><span>&#174;</span> Pepper All Rights Reserved.</small>
+                                <small className="rights"><span>&#174;</span> All Rights Reserved 2020-2021</small>
                             </div>
                             <div className="col-xl-2 col-md-4 col-sm-4 col-auto order-1 align-self-end ">
-                                <h6 className="mt-55 mt-2 text-muted bold-text"><b>ANIRUDH SINGLA</b></h6><small> <span><i
-                                className="fa fa-envelope" aria-hidden="true"></i></span> nhatbaovip@gmail.com</small>
+                                <h6 className="mt-55 mt-2 text-muted bold-text"><b>Contacts</b></h6><small> <span><i
+                                className="fa fa-envelope" aria-hidden="true"></i></span> 	chien8b123@gmail.com </small>
                             </div>
-                            <div className="col-xl-2 col-md-4 col-sm-4 col-auto order-2 align-self-end mt-3 ">
-                                <h6 className="text-muted bold-text"><b>RISHABH SHEKHAR</b></h6><small><span><i
-                                className="fa fa-envelope" aria-hidden="true"></i></span> rishab@gmail.com</small>
-                            </div>
+
                         </div>
                     </div>
                 </div>
--- src/app/homepage/HomePageContainer.js
+++ src/app/homepage/HomePageContainer.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import { Input, Table, Space } from "antd";
-import { SearchOutlined } from '@ant-design/icons';
+import "./HomePageContainer.css"
 
 const HomePageContainer = (props)=>{
     return(
-        <div></div>
+        <div className="hm-pg ">
+
+            <div class="row1">
+                <div className="hm-pg-text1 col-xl-7 col-sm 12  ">
+                    <p>Stop Buying. <strong>Start Trading</strong>.</p>
+                </div>
+            </div>
+
+            <div class="row2">
+                <div className="hm-pg-text2 col-xl-6 col-sm 12">
+                    <p>
+                        <span>
+                            <strong>Save Money</strong><strong>. Help the Environment.</strong>
+                        </span>
+                    </p>
+                    <p><span >People want your stuff!</span></p>
+
+                </div>
+
+            </div>
+        </div>
     )
 }
 export default HomePageContainer
